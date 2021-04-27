d079c8ed15b88f8ade905aca6fc2722677bd30d7
hyun8997 <33082198+hyun8997@users.noreply.github.com>
2021-04-27 17:00:52 +0900

    Update views.py

--- Django/ExcelCalculate/main/views.py
+++ Django/ExcelCalculate/main/views.py
@@ -48,3 +48,61 @@ def join(request):
 
     #return redirect('main_verifyCode')  # 요청 출력하고 이메일 코드확인 페이지까지
     #return response
+
+def verify(request):
+    user_code = request.POST['verifyCode']  # verifyCode.html에서 저 이름으로 넘어올 것임
+    cookie_code = request.COOKIES.get('code')  # 쿠키에 가입때 넣어둔 랜덤 코드 가져오기
+
+    if user_code == cookie_code:
+        user = User.objects.get(id=request.COOKIES.get('user_id'))
+        user.user_validate = 1
+        user.save()
+        response = redirect('main_index')
+        response.delete_cookie('code')
+        response.delete_cookie('user_id')
+        # response.set_cookie('user', user)  # 이제 가입 처리 후, 로그인 정보를 쿠키말고 세션처리하기로
+
+        request.session['user_email'] = user.user_email  # 회원 가입과 동시에 세션에 넣기
+        request.session['user_name'] = user.user_name
+
+        return response
+    else:
+        redirect('main_verifyCode')
+
+def logout(request):
+    del request.session['user_email']  # 세션에서 삭제
+    del request.session['user_name']
+    return redirect('main_signin')
+
+def login(request):
+    loginEmail = request.POST['loginEmail']  # signin.html에서 name대로 가져옴
+    loginPw = request.POST['loginPW']
+
+    # 회원등록여부 체크
+    try:
+        user = User.objects.get(user_email=loginEmail)  # 없으면 에러날거니까 에러처리로
+    except Exception as e:
+        print('e:', e)
+        return redirect('main_loginFail')
+
+    # 이메일 체크해서 있으면 넘어오는 곳임, 없으면 에러처리로 실패화면이동함
+    # 비밀번호 일치 여부
+    if user.user_password == loginPw:
+        request.session['user_name'] = user.user_name
+        request.session['user_email'] = user.user_email
+        return redirect('main_index')
+    else:
+        return redirect('main_loginFail')
+
+def loginFail(request):
+    return render(request, 'main/loginFail.html')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
