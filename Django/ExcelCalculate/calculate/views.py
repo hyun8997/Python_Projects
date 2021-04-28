4096eb1d3a94877e19fe0ee1a938c16288dbfb88
hyun8997 <33082198+hyun8997@users.noreply.github.com>
2021-04-28 10:14:29 +0900

    Add files via upload

--- Django/ExcelCalculate/calculate/views.py
+++ Django/ExcelCalculate/calculate/views.py
@@ -17,27 +17,64 @@ def calculate(request): #요청만 열어둠
     # header
     # print(df.head(5))  # 적당히 5개 잘라서 출력
 
-    # grade별 value의 최소값, 최대값, 평균값
+    # grade별 value 의 최소값, 최대값, 평균값
     # grade별 구분
     grade_dic = {}
-    total_row_num = len(df.index)  # 길이 구함
+    total_row_num = len(df.index)
 
-    # for i in range(total_row_num):
-    #     data = df.loc[[i], ['grade'], ['name'], ['']]
-    #
-    #     if not data['grade'] in grade_dic.keys():  # 데이터는 컬럼 이름으로 찾아올 수 있음
-    #         grade_dic[data['grade']] = [data['value']]  # 키(grade)마다 값(value) 넣어줌
-    #     else:
-    #         grade_dic[data['grade']].append(data['value'])
-    #
-    # # grade별 value의 최소값, 최대값, 평균값
-    # grade_calculate_dic = {}
-    # for key in grade_dic.keys():
-    #     grade_calculate_dic[key] = {}
-    #     grade_calculate_dic[key]['min'] = min(grade_dic[key])
-    #     grade_calculate_dic[key]['max'] = max(grade_dic[key])
-    #     grade_calculate_dic[key]['avg'] = float(sum(grade_dic[key])/len(grade_dic[key]))
+    for i in range(total_row_num):
+        data = df.loc[i]
 
+        if not data['grade'] in grade_dic.keys():
+            grade_dic[data['grade']] = [data['value']]
+        else:
+            grade_dic[data['grade']].append(data['value'])
 
+        print(grade_dic.keys(), '--', grade_dic.values())
+
+    # # grade별 value 의 최소값, 최대값, 평균값
+    grade_calculate_dic = {}
+    for key in grade_dic.keys():
+        grade_calculate_dic[key] = {}
+        grade_calculate_dic[key]['min'] = min(grade_dic[key])
+        grade_calculate_dic[key]['max'] = max(grade_dic[key])
+        grade_calculate_dic[key]['avg'] = float(sum(grade_dic[key])/len(grade_dic[key]))
+
+    #print(list(grade_calculate_dic.keys()))
+
+    print('------------------------------')
+    grade_list = list(grade_calculate_dic.keys())
+    grade_list.sort()   # 딕셔너리라서 key의 순서가 보장되지 않음
+
+    for key in grade_list:
+        print('--- grage --- ', key)
+        print('-- min : ', grade_calculate_dic[key]['min'], end=' ')
+        print('-- max : ', grade_calculate_dic[key]['max'], end=' ')
+        print('-- avg : ', grade_calculate_dic[key]['avg'], end='\n')
+
+    print('------------------------------------')
+
+    # 이메일 도메인별 인원 구하기
+    domain_dic = {}
+
+    for i in range(total_row_num):
+        data = df.loc[i]
+        # pos = data['email'].find('@')    # 데이터에서 email 중 @ 이후의 도메인 수집
+        # domain_data = data['email'][pos+1:]
+        domain_data = (data['email'].split('@'))[1]  # 데이터에서 email 중 @ 이후의 도메인 수집
+
+        if not domain_data in domain_dic.keys():
+            domain_dic[domain_data] = 1
+        else:
+            domain_dic[domain_data] = domain_dic[domain_data] + 1
+
+        print(domain_dic.keys(), '--', domain_dic.values())
+
+    for key in domain_dic.keys():   # 도메인 딕셔너리 출력
+        print(key, ':', domain_dic[key])
+
+    for key in domain_dic.keys():  # 2개 이상인 도메인 출력하기
+        if domain_dic[key] > 1:
+            print(key)
 
     return HttpResponse('calculate views - calculate function()')
